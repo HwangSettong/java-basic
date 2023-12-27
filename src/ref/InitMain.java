dde844fd785347e8ff497c393d6e895261affa34
HwangSettong <iamsehyun@gmail.com>
2023-12-27 23:45:46 +0900

    section2-2

---
+++ src/ref/InitMain.java
@@ -0,0 +1,9 @@
+package ref;
+
+public class InitMain {
+    public static void main(String[] args) {
+        InitData data = new InitData();
+        System.out.println("vlalue1 = " + data.value1);
+        System.out.println("vlalue2 = " + data.value2);
+    }
+}
